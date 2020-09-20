1ec666eb7a036a60972864c2ef2a44e8f565d652
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-20 11:50:39 +0200

    ArrayList size Scanner

---
+++ App.java
@@ -0,0 +1,20 @@
+package be.intecbrussel.opdrachten.opdracht1;
+
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Collection<Integer> userNumbers = new ArrayList<>();
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Geef in 5 nummer aub");
+
+        while (userNumbers.size() < 5) {
+            userNumbers.add(keyboard.nextInt());
+        }
+
+        userNumbers.forEach(System.out::println);
+    }
+}
